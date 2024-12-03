6404254aad20c67f5ad8b68e94635e2deba26928
Andrea Gandino <andreagandino@gmail.com>
2024-12-03 13:27:27 +0100

    Update

--- php-cs-fixer.dist.wordpress.php
+++ php-cs-fixer.dist.wordpress.php
@@ -4,6 +4,10 @@ if ( file_exists( __DIR__ . "/vendor/autoload.php" ) ) {
 	require_once __DIR__ . "/vendor/autoload.php";
 }
 
+if ( file_exists( __DIR__ . "/../../autoload.php" ) ) {
+	require_once __DIR__ . "/../../autoload.php";
+}
+
 return ( new PhpCsFixer\Config() )
 	->setRiskyAllowed( true )
 	->registerCustomFixers( [
