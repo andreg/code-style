db30705588e715690decddbbc2a8165204441830
Andrea Gandino <andreagandino@gmail.com>
2026-05-05 09:57:36 +0200

    Allow support for newer PHP versions

--- .vscode/php-cs-fixer.dist.php
+++ .vscode/php-cs-fixer.dist.php
@@ -1,3 +1,9 @@
 <?php
 
-return include __DIR__ . '/../php-cs-fixer.dist.php';
+$config = include __DIR__ . '/../php-cs-fixer.dist.php';
+
+if ( method_exists( $config, 'setUnsupportedPhpVersionAllowed' ) ) {
+	$config->setUnsupportedPhpVersionAllowed( true );
+}
+
+return $config;
